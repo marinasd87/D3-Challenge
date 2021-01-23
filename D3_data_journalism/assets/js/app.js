90fc3ac9aa649dc5c5dcbf94769c58d0efde0cc2
Marina Duarte <marinaduarte@Marinas-MBP.attlocal.net>
2021-01-23 14:01:39 -0600

    app

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -6,14 +6,14 @@ var margin = {
   top: 20,
   right: 40,
   bottom: 60,
-  left: 100
+  left: 50
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 // Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select(".chart")
+var svg = d3.select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -22,19 +22,19 @@ var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 // Load data datacsv
-d3.csv("./data/data.csv").then(function(stateData) {
+d3.csv("/Documents/D3-Challenge/D3_data_journalism/assets/data/data.csv").then(function(stateData) {
 
     console.log(stateData);
   
     // log a list of states
-    var states = stateData.map(data => data.state);
+    var states = stateData.map(data => data.abbr);
     console.log("states", states);
   
     // Cast each poverty and obesity value in stateData as a number using the unary + operator
     stateData.forEach(function(data) {
       data.poverty = +data.poverty;
       data.obesity = +data.obesity;
-      console.log("State:", data.state);
+      console.log("State:", data.abbr);
       console.log("Poverty:", data.poverty);
       console.log("Obesity:", data.obesity);
     });
@@ -42,11 +42,11 @@ d3.csv("./data/data.csv").then(function(stateData) {
     // Step 2: Create scale functions
     // ==============================
     var xLinearScale = d3.scaleLinear()
-      .domain([20, d3.max(stateData, d => d.obesity)])
+      .domain([20, d3.max(stateData, d => d.obesity)+2])
       .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-      .domain([0, d3.max(stateData, d => d.poverty)])
+      .domain([8, d3.max(stateData, d => d.poverty)+2])
       .range([height, 0]);
 
     // Step 3: Create axis functions
@@ -72,8 +72,14 @@ d3.csv("./data/data.csv").then(function(stateData) {
     .attr("cx", d => xLinearScale(d.obesity))
     .attr("cy", d => yLinearScale(d.poverty))
     .attr("r", "15")
-    .attr("fill", "pink")
+    .attr("fill", "blue")
     .attr("opacity", ".5");
+    var textGroup = chartGroup.selectAll("text")
+    .data(stateData)
+    .enter()
+    .append("text").text(d => d.abbr)
+    .attr("x", d => xLinearScale(d.obesity)-11)
+    .attr("y", d => yLinearScale(d.poverty)+5);
 
     // Step 6: Initialize tool tip
     // ==============================
@@ -101,16 +107,16 @@ d3.csv("./data/data.csv").then(function(stateData) {
     // Create axes labels
     chartGroup.append("text")
       .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left + 40)
+      .attr("y", 0 - margin.left + 1)
       .attr("x", 0 - (height / 2))
       .attr("dy", "1em")
       .attr("class", "axisText")
-      .text("Poverty");
+      .text("Poverty (%)");
 
     chartGroup.append("text")
       .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
       .attr("class", "axisText")
-      .text("Obesity");
+      .text("Obesity(%)");
 
   }).catch(function(error) {
     console.log(error);
